aed02444509898c78deebf38803f2e9f539d9cfe
meiis <rewerewe113@gmail.com>
2020-03-28 23:24:15 +0900

    Reusable Component with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+function Food(props) {
+  console.log(props);
+  return <h1>I like Yogurt</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      
+      <Food fav="kimchi" name="food" />
     </div>
   ); 
    
