4376e23d97245f95bb7a8b5ede9882f8ee93906a
meiis <rewerewe113@gmail.com>
2020-04-01 11:04:09 +0900

    Add setTimeout on componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,26 +3,19 @@ import React from "react";
 class App extends React.Component {
   
   state = {
-    count: 0
+    isLoading: true
   };
 
-  // state를 set할때, react에서 외부의 상태에 의존하지 않는 가장 좋은 방법! 
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  }
-
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading: false});  
+    }, 6000); 
   }
 
   render() {
-    return (
-        <div>
-          <h1>The number is: {this.state.count}</h1>
-          <button onClick={this.add}>Add</button>
-          <button onClick={this.minus}>Minus</button>
-        </div>
-      );
+    const { isLoading } = this.state;
+    return <div>{ isLoading ? "Loading..." : "we are ready"}</div>;
+    
   }  
 }
 
